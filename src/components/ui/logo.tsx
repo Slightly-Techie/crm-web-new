eac2aba8fd2a0186052dadf615e7044941a72c7b
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-05-17 10:43:29 +0000

    feat: progress on auth flow

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="bg-gray-600 p-2 mx-auto rounded-sm text-[white]">
+      <h5 className="font-bold">ST</h5>
+    </div>
+  );
+}
+
+export default Logo;
